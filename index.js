e0ccb8416639ae2667f96d84cb95613051970329
gilangaryo <gilang91668@gmail.com>
2024-05-02 17:48:29 +0700

    add database

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
-
+const supabase = require('@supabase/supabase-js');
 const app = express();
 const PORT = 4000;
 
+const SUPABASE_URL = "https://jnrcmmxijleugueubrnu.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpucmNtbXhpamxldWd1ZXVicm51Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDY0NTU1NywiZXhwIjoyMDMwMjIxNTU3fQ.0SSmPw3cpCE60SJUNjaJBrkiT1okIxhfu8h5-AtG4_w";
+
+const db = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 app.use(express.json());
-app.get('/', (req, res) => {
-    console.log("haii!");
-    res.send('halo bang!');
+app.get('/', async (req, res) => {
+    const getData = await db.from("sapi").select();
+    console.log(getData);
+    res.json({ getData });
 });
 
-app.post('/', (req, res) => {
-    console.log("halo bang masuk ya bang!");
-    const responseBody = req.body;
-    console.log(responseBody);
-    res.send(responseBody);
+app.post('/', async (req, res) => {
+    const { nama, uid } = req.body;
+    const tambahData = await db.from("sapi").insert({ nama, uid })
+    console.log("halo bang masuk ya bang! ", tambahData);
+    res.json({ tambahData });
 });
 
 
