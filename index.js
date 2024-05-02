0672fa370119a36b95b44a6c48e2f77d1bdaf6d0
gilangaryo <gilang91668@gmail.com>
2024-05-02 16:46:55 +0700

    req body

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 const PORT = 4000;
 
-app.use(cors());
+app.use(express.json());
 app.get('/', (req, res) => {
     console.log("haii!");
     res.send('halo bang!');
 });
 
-const api_key_value = '54321s';
-
-
-
 app.post('/', (req, res) => {
-    res.send(req.body);
-    // const api_key = req.body.api_key;
-    // if (api_key === api_key_value) {
-    //     res.send(req.body);
-
-    // } else {
-    //     res.status(403).send('Wrong API Key');
-    // }
+    console.log("halo bang masuk ya bang!");
+    const responseBody = req.body;
+    console.log(responseBody);
+    res.send(responseBody);
 });
+
 
 app.listen(PORT, () => {
     console.log('Example app listening on port', PORT);
